2063e1b1c64220c6d749b259227e0ff6fd421e6f
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-01-03 22:29:47 -0300

    criando funcionalidade de login com firebase

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,17 +1,53 @@
 import React, { useState } from 'react';
-import { View, Text, SafeAreaView, Image } from 'react-native';
+import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import firebase from 'react-native-firebase';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Input from '../../components/input';
 import styles from './styles'
 
 
-const Login = () => {
+const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+console.log(props)
+    const login = async() =>{
+        //console.log(email, senha)
+        try {
+            const {user} = await firebase.auth().signInWithEmailAndPassword(email, senha);
+           // console.log(user)
+            await AsyncStorage.setItem('user', user.email);
+            props.navigation.dispatch(NavigationActions.navigate({
+                routeName: 'Home'
+              }))
+            
+        } catch (error) {
+           // console.log(error);
+        }
+       
+    };
+
+    const navigateToScreen = (route) => () => {
+        const navigateAction = NavigationActions.navigate({
+          routeName: route
+        }); 
+        props.navigation.dispatch(navigateAction);
+    };
     return(
         <SafeAreaView style={styles.container}>
             <Image source={require('../../../assets/img/cte.png')} style={styles.img}/>
-            <Input placeholder="Nome"/>
-            <Input placeholder="Email"/>
-                    
+            <Input placeholder="Email" keyboardType='email-address' value={email} onChangeText={email => setEmail(email)}/>
+            <Input placeholder="Senha" secureTextEntry={true} value={senha} onChangeText={senha => setSenha(senha)}/>
+            <TouchableOpacity style={styles.button} onPress={login}>
+                <Text style={styles.buttonText}>Logar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity  onPress={navigateToScreen('Registro')}>
+                <Text style={styles.links}>Registre-se</Text>
+            </TouchableOpacity>
+            <TouchableOpacity  onPress={()=>{}}>
+                <Text style={styles.links}>Escqueceu a senha?</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
    
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -9,10 +9,28 @@ const styles = StyleSheet.create({
         
     },
     img:{
-        width:200, 
-        height:90,
-        marginBottom: 60
-    }
+        width:100, 
+        height:45,
+        marginBottom: 10
+    },
+    button:{
+        height: 45,
+        backgroundColor: '#069',
+        alignSelf: 'stretch',
+        paddingHorizontal: 20,
+        marginLeft: 16,
+        marginRight: 16,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText:{
+        color: '#fff',
+        fontWeight: 'bold'
+    },
+    links:{
+        paddingTop: 16,
+        color: '#069',
+    },
 });
 
 export default styles;
