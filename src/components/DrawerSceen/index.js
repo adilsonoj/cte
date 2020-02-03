d738b0957f6196f14ca123f726b37723bf42f947
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-02-03 18:57:55 -0300

    fix: limpar estado redux ao fazer logout

--- src/components/DrawerSceen/index.js
+++ src/components/DrawerSceen/index.js
@@ -2,76 +2,86 @@ import React, { Component } from 'react';
 
 import { ScrollView, Text, View, StatusBar } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
-import { DrawerActions } from 'react-navigation-drawer';
+import { DrawerActions, DrawerRouter } from 'react-navigation-drawer';
 import { NavigationActions,  SwitchActions } from 'react-navigation';
 import auth from '@react-native-firebase/auth';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Theme from '../../themes/white';
 import styles from './styles';
 
-export default class DrawerScreen extends Component {
-    navigateToScreen = (route) => () => {
+import { connect } from 'react-redux';
+import * as LoginAction from '../../actions/loginAction';
+import { bindActionCreators } from 'redux';
+
+const DrawerScreen = (props)=> {
+
+    const { updateUserLogged, navigation  } = props; 
+    const navigateToScreen = (route) => () => {
         const navigateAction = NavigationActions.navigate({
           routeName: route
         }); 
-        this.props.navigation.dispatch(navigateAction);
-        this.props.navigation.dispatch(DrawerActions.closeDrawer())
+        navigation.dispatch(navigateAction);
+        navigation.dispatch(DrawerActions.closeDrawer())
     }
 
-    logout = (route) => async () => {
+    const logout = (route) => async () => {
       const user = auth().currentUser;
 
       if (user) {
         await auth().signOut();
         await AsyncStorage.clear();
+        updateUserLogged({});
       }
-      this.props.navigation.dispatch( NavigationActions.navigate({ routeName: route }));
+
+      navigation.dispatch( NavigationActions.navigate({ routeName: route }));
     }
 
-  render() {
    
     return (
       
     <View>
         <StatusBar backgroundColor={Theme.primaryDark} barStyle="light-content" />
         <ScrollView>
-          <View style={[styles.item, (this.props.activeItemKey=='CTE' ? styles.activeItem : null)]} >
-            <Icon name='home' size={20} style={[styles.icon, (this.props.activeItemKey=='CTE' ? styles.activeIcon : null)]} />
-            <Text style={[styles.text, (this.props.activeItemKey=='CTE' ? styles.activeText : null)]} onPress={this.navigateToScreen('CTE')}>
+          <View style={[styles.item, (props.activeItemKey=='CTE' ? styles.activeItem : null)]} >
+            <Icon name='home' size={20} style={[styles.icon, (props.activeItemKey=='CTE' ? styles.activeIcon : null)]} />
+            <Text style={[styles.text, (props.activeItemKey=='CTE' ? styles.activeText : null)]} onPress={navigateToScreen('CTE')}>
               Home
             </Text>
           </View>
 
-          <View  style={[styles.item, (this.props.activeItemKey=='Perfil' ? styles.activeItem : null)]} >
-            <Icon name='user' size={20} style={[styles.icon, (this.props.activeItemKey=='Perfil' ? styles.activeIcon : null)]} />
-            <Text style={[styles.text, (this.props.activeItemKey=='Perfil' ? styles.activeText : null)]} onPress={this.navigateToScreen('Perfil')}>
+          <View  style={[styles.item, (props.activeItemKey=='Perfil' ? styles.activeItem : null)]} >
+            <Icon name='user' size={20} style={[styles.icon, (props.activeItemKey=='Perfil' ? styles.activeIcon : null)]} />
+            <Text style={[styles.text, (props.activeItemKey=='Perfil' ? styles.activeText : null)]} onPress={navigateToScreen('Perfil')}>
               Perfil
             </Text>
           </View>
           
-          <View  style={[styles.item, (this.props.activeItemKey=='Sobre' ? styles.activeItem : null)]} >
-            <Icon name='address-card' size={20} style={[styles.icon, (this.props.activeItemKey=='Sobre' ? styles.activeIcon : null)]} />
-            <Text style={[styles.text, (this.props.activeItemKey=='Sobre' ? styles.activeText : null)]} onPress={this.navigateToScreen('Sobre')}>
+          <View  style={[styles.item, (props.activeItemKey=='Sobre' ? styles.activeItem : null)]} >
+            <Icon name='address-card' size={20} style={[styles.icon, (props.activeItemKey=='Sobre' ? styles.activeIcon : null)]} />
+            <Text style={[styles.text, (props.activeItemKey=='Sobre' ? styles.activeText : null)]} onPress={navigateToScreen('Sobre')}>
               Sobre
             </Text>
           </View>
 
-          <View style={[styles.item, (this.props.activeItemKey=='Settings' ? styles.activeItem : null)]} >
-            <Icon name='cog' size={20} style={[styles.icon, (this.props.activeItemKey=='Settings' ? styles.activeIcon : null)]} />
-            <Text style={[styles.text, (this.props.activeItemKey=='Settings' ? styles.activeText : null)]} onPress={this.navigateToScreen('Settings')}>
+          <View style={[styles.item, (props.activeItemKey=='Settings' ? styles.activeItem : null)]} >
+            <Icon name='cog' size={20} style={[styles.icon, (props.activeItemKey=='Settings' ? styles.activeIcon : null)]} />
+            <Text style={[styles.text, (props.activeItemKey=='Settings' ? styles.activeText : null)]} onPress={navigateToScreen('Settings')}>
               Configurações
             </Text>
           </View>
 
-          <View  style={[styles.item, (this.props.activeItemKey=='Lggin' ? styles.activeItem : null)]} >
-            <Icon name='sign-out' size={20} style={[styles.icon, (this.props.activeItemKey=='Login' ? styles.activeIcon : null)]} />
-            <Text style={[styles.text, (this.props.activeItemKey=='Login' ? styles.activeText : null)]} onPress={this.logout('Login')}>
+          <View  style={[styles.item, (props.activeItemKey=='Lggin' ? styles.activeItem : null)]} >
+            <Icon name='sign-out' size={20} style={[styles.icon, (props.activeItemKey=='Login' ? styles.activeIcon : null)]} />
+            <Text style={[styles.text, (props.activeItemKey=='Login' ? styles.activeText : null)]} onPress={logout('Login')}>
             Sair
             </Text>
           </View>
         </ScrollView>
       </View>);
   }
-}
 
 
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(LoginAction, dispatch);
+
+export default connect('', mapDispatchToProps)(DrawerScreen);
