1fbe45d7a54ca4aaa9b07aae4add65f401b34b24
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-02-12 12:49:14 -0300

    feat: verificaçao com redux

--- src/pages/LoginOrHome/index.js
+++ src/pages/LoginOrHome/index.js
@@ -1,42 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, {useEffect, useState} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import * as LoginAction from '../../actions/loginAction';
 import Loader from '../../components/Loader';
 
-const LoginOrHome = (props)=>{
-    const [loading, setLoading] = useState(true);
+const LoginOrHome = props => {
+  const [loading, setLoading] = useState(true);
 
-    useEffect( ()=>{
-      getUser();
-    },[])
+  useEffect(() => {
+    getUser();
+  }, []);
 
-    const getUser = async ()=>{
-      try {
-            console.log(props.user)
-           if(Object.entries(props.user).length !== 0){
-              props.navigation.navigate("Home");
-
-           }else{
-
-              props.navigation.navigate("Login");
-           }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
+  const getUser = async () => {
+    try {
+      console.log(props.user);
+      if (props.user && Object.entries(props.user).length !== 0) {
+        props.navigation.navigate('Home');
+      } else {
+        props.navigation.navigate('Login');
       }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-    
-    return <Loader loading={loading}/>
-   
+  };
+
+  return <Loader loading={loading} />;
 };
 
 const mapStateToProps = state => ({
-  user: state.userLogged.user
+  user: state.userLogged.user,
 });
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(LoginAction, dispatch);
 
-export default connect(mapStateToProps,mapDispatchToProps)(LoginOrHome);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginOrHome);
