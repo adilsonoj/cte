54147fa09e9d7ec267ae4d236731c86681e36cdd
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-02-12 12:48:26 -0300

    refect: alterado espaçamento do label

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,57 +1,63 @@
-import React, { useState }from 'react';
-import { View, Text, TextInput, Animated } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, TextInput, Animated} from 'react-native';
 import theme from '../../themes/white';
-import styles from  './styles';
+import styles from './styles';
 
-const Input= (props) => {
-    const [ isFocused, setFocused ] = useState(false);
-    const [ opacity ] = useState(new Animated.Value(0));
-    const [ translateY ] = useState(new Animated.Value(8));
-    
-    const handleFocus = ()=>{
-        setFocused(true);
-        Animated.timing(opacity, {
-            toValue: 1,
-            duration: 500
-         }).start();
-         Animated.timing(translateY, {
-            toValue: -8,
-            duration: 500
-         }).start();
-    }
+const Input = props => {
+  const [isFocused, setFocused] = useState(false);
+  const [opacity] = useState(new Animated.Value(0));
+  const [translateY] = useState(new Animated.Value(2));
 
-    const handleBlur = ()=>{
-        setFocused(false)
-        Animated.timing(opacity, {
-            toValue: 0,
-            duration: 0
-         }).start();
-         Animated.timing(translateY, {
-            toValue: 8,
-            duration: 0
-         }).start();
-    }
+  const handleFocus = () => {
+    setFocused(true);
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 500,
+    }).start();
+    Animated.timing(translateY, {
+      toValue: -2,
+      duration: 500,
+    }).start();
+  };
+
+  const handleBlur = () => {
+    setFocused(false);
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 0,
+    }).start();
+    Animated.timing(translateY, {
+      toValue: 2,
+      duration: 0,
+    }).start();
+  };
   return (
     <View style={styles.container}>
-        <Animated.Text style={[styles.text, {opacity: opacity, transform: [{translateY: translateY}] }]}>{props.placeholder}</Animated.Text>
-        <TextInput 
-            style={ [styles.textInput, props.width ? { width: props.width } : {}] } 
-            placeholder={ props.placeholder}
-            underlineColorAndroid={ isFocused ? theme.primary : '#ccc' }
-            onFocus={ handleFocus }
-            onBlur={ handleBlur }
-            secureTextEntry={ props.secureTextEntry }
-            autoCorrect={ props.autoCorrect }
-            onChangeText={ props.onChangeText }
-            value={ props.value }
-            keyboardType={ props.keyboardType }
-            secureTextEntry = {props.secureTextEntry}
-            autoFocus = {props.autoFocus}
-            multiline = {props.multiline} 
-            numberOfLines = {props.numberOfLines}
-        />
+      <Animated.Text
+        style={[
+          styles.text,
+          {opacity: opacity, transform: [{translateY: translateY}]},
+        ]}>
+        {props.placeholder}
+      </Animated.Text>
+      <TextInput
+        style={[styles.textInput, props.width ? {width: props.width} : {}]}
+        placeholder={props.placeholder}
+        underlineColorAndroid={isFocused ? theme.primary : '#f5f5f5'}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        secureTextEntry={props.secureTextEntry}
+        autoCorrect={props.autoCorrect}
+        onChangeText={props.onChangeText}
+        value={props.value}
+        keyboardType={props.keyboardType}
+        secureTextEntry={props.secureTextEntry}
+        autoFocus={props.autoFocus}
+        multiline={props.multiline}
+        numberOfLines={props.numberOfLines}
+      />
     </View>
   );
-}
+};
 
 export default Input;
--- src/components/input/styles.js
+++ src/components/input/styles.js
@@ -1,18 +1,17 @@
-import { StyleSheet, Dimensions } from 'react-native'
-import theme from '../../themes/white'
-
+import {StyleSheet, Dimensions} from 'react-native';
+import theme from '../../themes/white';
 
 const styles = StyleSheet.create({
-    container:{
-        paddingVertical: 6,
-    },
-    textInput:{
-        height: 40,
-        fontSize: 16,
-    },
-    text:{
-        color: theme.primary,
-        paddingLeft: 6,
-    }
-})
+  container: {
+    paddingVertical: 0,
+  },
+  textInput: {
+    height: 40,
+    fontSize: 16,
+  },
+  text: {
+    color: theme.primary,
+    paddingLeft: 6,
+  },
+});
 export default styles;
