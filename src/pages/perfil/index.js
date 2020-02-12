17278e294531d48f9f949c040f9e11b00f9169e1
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-02-12 12:50:45 -0300

    feat: adicionado novo objetivo

--- src/pages/perfil/index.js
+++ src/pages/perfil/index.js
@@ -1,101 +1,216 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
-import { View, Text, SafeAreaView, ScrollView, Button, TouchableWithoutFeedback } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Button,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import AsyncStorage from '@react-native-community/async-storage';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
+import {connect} from 'react-redux';
+import * as LoginAction from '../../actions/loginAction';
+import {bindActionCreators} from 'redux';
 import styles from './styles';
 import Input from '../../components/input';
 import ImagemPerfil from '../../components/imagemPerfil';
-import theme from '../../themes/white'
+import theme from '../../themes/white';
 import moment from 'moment';
 require('moment/locale/pt-br');
 
-const perfil = () => {
-    const [ user, setUser ] = useState({});
-    const [ objetivo, setObjetivo ] = useState({});
-    const [nome, setNome] = useState('');
-    const [email, setEmail] = useState('');
-    const [idade, setIdade] = useState('');
-    const [peso, setPeso] = useState('');
-    const [distancia, setDistancia] = useState('');
-    const [data, setData] = useState(new Date());
-    const [show, setShow] = useState(false);
-    const [observacao, setObservacao] = useState('');
-    const [initializing, setInitializing] = useState(true);
-    const uid = auth().currentUser.uid;
-    const onLoad = async ()=>{
-        let userStorage = await AsyncStorage.getItem('user');
+const perfil = ({userStore, updateUserLogged, navigation}) => {
+  const [user, setUser] = useState({});
+  const [objetivo, setObjetivo] = useState({});
+  const [objetivos, setObjetivos] = useState([]);
+  const [data, setData] = useState(new Date());
+  const [show, setShow] = useState(false);
+  const [novo, setNovo] = useState(false);
+  const uid = auth().currentUser && auth().currentUser.uid;
+  const onLoad = async () => {
+    console.log('@@', userStore);
 
-        //const userDoc = await firestore().doc(`users/${uid}`).get()
-        console.log('@@',userStorage)
-      // userDoc._data ? setUser( { ...userStorage, ...userDoc._data }) : 
-        
-       setUser( {...userStorage} )
-       console.log(user)
+    if (userStore.objetivos) {
+      let d = userStore.objetivos.find((e, i) => i == 0);
+
+      let dataF = moment(d.data._seconds * 1000).format(
+        'D [de] MMMM [de] YYYY',
+      );
+      // setObjetivo({...d, data: dataF});
+      setObjetivos(userStore.objetivos);
+      delete userStore.objetivos;
+      setUser({...userStore, ...d, data: dataF});
+    } else {
+      setUser(userStore);
+    }
+  };
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  const changeData = (event, date) => {
+    date = date || data;
+    setShow(false);
+    setData(date);
+    let dataF = moment(date).format('D [de] MMMM [de] YYYY');
+    // setObjetivo({...objetivo, data: dataF});
+    setUser({...user, data: dataF});
+  };
+  const onUpdate = async () => {
+    // await auth().currentUser.updateProfile({
+    //     photoURL: '',
+    // });
+
+    if (novo) {
+      objetivos.unshift({
+        distancia: user.distancia,
+        observacao: user.observacao,
+        data,
+      });
+    } else if (objetivos.length > 0) {
+      let auxObj = objetivos.find((e, i) => i == 0);
+      objetivos.shift();
+      auxObj.distancia = user.distancia;
+      auxObj.observacao = user.observacao;
+      auxObj.data = data;
+      objetivos.unshift(auxObj);
+    } else {
+      objetivos.push({
+        distancia: user.distancia,
+        observacao: user.observacao,
+        data: user.data,
+      });
     }
 
-    useEffect(()=>{
-        onLoad();
-    },[]);
+    // let obj = [
+    //   {
+    //     distancia: user.distancia,
+    //     observacao: user.observacao,
+    //     data,
+    //   },
+    // ];
+    console.log(objetivos.length);
+    console.log(novo);
+    obj = objetivos;
+    let dados = {
+      idade: user.idade,
+      peso: user.peso,
+      objetivos: obj,
+    };
 
-  
-
-    const changeData = (event, date)=>{
-        date = date || data;
-        setShow(false);
-        setObjetivo({ ...objetivo,   date });
-        setUser({...user, objetivo})
+    const userDoc = await firestore().doc(`users/${uid}`);
+    try {
+      await userDoc.set(dados, {merge: true});
+      const userF = await firestore()
+        .doc(`users/${uid}`)
+        .get();
+      console.log('snapshot', userF._data);
+      updateUserLogged({...user, ...userF._data});
+      console.log('salvo com sucesso');
+      Alert.alert('', 'Perfil salvo');
+    } catch (error) {
+      console.log(error);
     }
-    const onUpdate = async () => {
-        // await auth().currentUser.updateProfile({
-        //     photoURL: '',
-        // });
-        setUser({ ...user, objetivo })
-        
-        const userDoc = await firestore().doc(`users/${uid}`);
-        if(!user)
+  };
 
+  const newObjetivo = async () => {
+    setNovo(true);
+    let userAux = user;
+    userAux.distancia = '';
+    userAux.observacao = '';
+    userAux.data = '';
+    setUser({...userAux});
+    let {
+      _data: {objetivos},
+    } = await firestore()
+      .doc(`users/${uid}`)
+      .get();
 
-        console.log(user)
-        await userDoc.set(user ,{merge: true});
-    }
-    
-    return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView >
-                    <View style={styles.imgPerfil}>
-                        <ImagemPerfil />
-                    </View>
-                    <Input placeholder="Nome" onChangeText={displayName => setUser({ ...user, displayName })} value={user.displayName}/>
-                    <Input placeholder="Email" onChangeText={email => setUser({ ...user, email })} value={user.email} keyboardType="email-address"/>
-                    <Input placeholder="Idade" onChangeText={idade => setUser({ ...user, idade })} value={user.idade} keyboardType="numeric"/>
-                    <Input placeholder="Peso (kg)" onChangeText={peso => setUser({...user, peso})} value={user.peso}  keyboardType="numeric"/>
-                    <View style={styles.header}>
-                        <Text >OBJETIVO</Text>
-                    </View>
-                    
-                    <Input placeholder="Distância (km)" onChangeText={distancia => setObjetivo({ ...objetivo, distancia })} value={user.distancia}  keyboardType="numeric"/>
-                    <Input placeholder="Observação" onChangeText={observacao => setObjetivo({ ...objetivo,  observacao })} value={user.observacao} multiline = {true} numberOfLines = {3}/>
-                    <TouchableWithoutFeedback onPress={()=>setShow(true)}>
-                        <View style={styles.card}> 
-                            <Text style={styles.text}>{moment(data).format('D [de] MMMM [de] YYYY')}</Text>
-                        </View>
-                    </TouchableWithoutFeedback>
-                    {/* <Input placeholder="Senha" secureTextEntry={true} autoCorrect={false}/> */}
-                    
-                
-                { show && <DateTimePicker value={data}
-                      is24Hour={true}
-                      display="default"
-                      onChange={changeData} />
-            }
-                <View style={styles.button}>
-                    <Button title="Salvar" color={theme.button} onPress={onUpdate}/>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    )};
+    setObjetivos(objetivos);
+    console.log(user);
+  };
 
-export default perfil;
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.imgPerfil}>
+          <ImagemPerfil />
+        </View>
+        <Input
+          placeholder="Nome"
+          onChangeText={displayName => setUser({...user, displayName})}
+          value={user.displayName}
+        />
+        <Input
+          placeholder="Email"
+          onChangeText={email => setUser({...user, email})}
+          value={user.email}
+          keyboardType="email-address"
+        />
+        <Input
+          placeholder="Idade"
+          onChangeText={idade => setUser({...user, idade})}
+          value={user.idade}
+          keyboardType="numeric"
+        />
+        <Input
+          placeholder="Peso (kg)"
+          onChangeText={peso => setUser({...user, peso})}
+          value={user.peso}
+          keyboardType="numeric"
+        />
+        <View style={styles.header}>
+          <Text>OBJETIVO</Text>
+          <TouchableOpacity onPress={newObjetivo}>
+            <Text>Novo</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Input
+          placeholder="Distância (km)"
+          onChangeText={distancia => setUser({...user, distancia})}
+          value={user.distancia}
+          keyboardType="numeric"
+        />
+        <Input
+          placeholder="Observação"
+          onChangeText={observacao => setUser({...user, observacao})}
+          value={user.observacao}
+          multiline={true}
+          numberOfLines={3}
+        />
+        <TouchableWithoutFeedback onPress={() => setShow(true)}>
+          <View style={styles.card}>
+            <Text style={[styles.text]}>{user.data}</Text>
+          </View>
+        </TouchableWithoutFeedback>
+
+        {show && (
+          <DateTimePicker
+            value={data}
+            is24Hour={true}
+            display="default"
+            onChange={changeData}
+          />
+        )}
+        <View style={styles.button}>
+          <Button title="Salvar" color={theme.button} onPress={onUpdate} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const mapStateToProps = state => ({
+  userStore: state.userLogged.user,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(LoginAction, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(perfil);
--- src/pages/perfil/styles.js
+++ src/pages/perfil/styles.js
@@ -1,36 +1,41 @@
-import { StyleSheet, Dimensions } from 'react-native'
-import theme from '../../themes/white'
+import {StyleSheet, Dimensions} from 'react-native';
+import theme from '../../themes/white';
 
 const styles = StyleSheet.create({
-    container:{
-       paddingVertical: 16,
-       paddingHorizontal: 16,
-    },
-    header:{
-        justifyContent:'center',
-        marginTop: 10,
-        marginBottom:10,
-        height: 30,
-        paddingLeft: 6,
-        backgroundColor: '#EEEEEE',
-    },
-    button:{
-        paddingVertical:16
-    },
-    card:{
-        flexDirection: "row",
-        alignItems: "flex-end",
-        height: 50,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-        paddingLeft: 6
-    },
-    imgPerfil:{
-        alignItems: 'center'
-    },
-    text: {
-        fontSize: 16
-    }
+  container: {
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+    height: 30,
+    paddingHorizontal: 6,
+    backgroundColor: '#EEEEEE',
+  },
+  button: {
+    paddingVertical: 16,
+  },
+  card: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    height: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f5f5f5',
+    paddingLeft: 6,
+  },
+  imgPerfil: {
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 16,
+  },
+  placeholder: {
+    color: '#aaa',
+  },
 });
 
 export default styles;
