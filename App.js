ffc49b4f86fe57db7300dcec9ac7c8faf76316e5
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-02-12 12:45:54 -0300

    feat: adicionado react-native-paper

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Provider } from 'react-redux';
-import { store, persistor } from './src/store';
+import React, {Component} from 'react';
+import {Provider as StoreProvider} from 'react-redux';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {store, persistor} from './src/store';
 import RootNavigation from './src/routes/RootNavigation';
-import { PersistGate } from 'redux-persist/integration/react'
-
+import {PersistGate} from 'redux-persist/integration/react';
+import Theme from './src/themes/white';
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: Theme.primary,
+    background: '#fff',
+  },
+};
 class App extends Component {
-    render(){
-        return(
-            <Provider store={store}>
-                <PersistGate loading={null} persistor={persistor}>
-                    <RootNavigation />
-                </PersistGate>
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <StoreProvider store={store}>
+        <PaperProvider theme={theme}>
+          <PersistGate loading={null} persistor={persistor}>
+            <RootNavigation />
+          </PersistGate>
+        </PaperProvider>
+      </StoreProvider>
+    );
+  }
 }
 
 export default App;
