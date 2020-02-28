25a4af043d8058972d5ef22135203148b2d78e71
Adilson <adilson@amrj.mar.mil.br>
2020-02-28 15:42:34 -0300

    feat: aicionado touch no input

--- src/components/InputPlanilha/index.js
+++ src/components/InputPlanilha/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import styles from './styles'
+import {View, Text, TouchableWithoutFeedback} from 'react-native';
+import styles from './styles';
 
 const InputPlanilha = props => {
   return (
-    <View style={styles.planilha}> 
-        <Text style={styles.planilhaTitulo}>{props.titulo}</Text>
-        <View style={styles.planilhaItem}>
-        <View style={styles.planilhaCampo}>
+    <View style={styles.planilha}>
+      <Text style={styles.planilhaTitulo}>{props.titulo}</Text>
+      <View style={styles.planilhaItem}>
+        <TouchableWithoutFeedback onPress={props.changeValue}>
+          <View style={styles.planilhaCampo}>
             <Text>{props.valor}</Text>
-        </View>
-        <View style={[styles.planilhaCampo, styles.borderLeft]}>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={props.changeRitimo}>
+          <View style={[styles.planilhaCampo, styles.borderLeft]}>
             <Text>{props.ritimo}</Text>
-        </View>
-        </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
-}
+};
 
 export default InputPlanilha;
