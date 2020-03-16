83d33ef3353ab0349f878a8567d853b3c265c6af
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-03-15 21:55:05 -0300

    style: aumenta tamanho dos cards

--- src/pages/professor/vo2/styles.js
+++ src/pages/professor/vo2/styles.js
@@ -1,40 +1,38 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 import theme from '../../../themes/white';
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-       backgroundColor: theme.background,
-       paddingHorizontal: 16,
-       paddingHorizontal: 16
-    },
+  container: {
+    flex: 1,
+    backgroundColor: theme.background,
+    paddingHorizontal: 16,
+    paddingHorizontal: 16,
+  },
 
-    variacoes:{
-        flexDirection: "row",
-        justifyContent: "space-around",
-        alignItems: "center",
-        height: 40,
-        backgroundColor: theme.backgroundCard,
-        ...theme.shadow
-    },
-    item:{
-        width: Dimensions.get("window").width / 3,
-        alignItems: "center",
-        
-    },
-    border:{
-        borderLeftWidth: 1,
-        borderRightWidth: 1
-    },
-    titulo:{
-        fontSize: 15,
-        fontWeight: "bold",
-        color: theme.primary
-    },
-    button:{
-        color: theme.button
-    }
-   
+  variacoes: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: 45,
+    backgroundColor: theme.backgroundCard,
+    ...theme.shadow,
+  },
+  item: {
+    width: Dimensions.get('window').width / 3,
+    alignItems: 'center',
+  },
+  border: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+  },
+  titulo: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: theme.primary,
+  },
+  button: {
+    color: theme.button,
+  },
 });
 
 export default styles;
