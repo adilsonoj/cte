14414a5c14d7fcb84eec1eb41395f675db6fd7c1
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-03-15 21:49:23 -0300

    feat: adiciona açao aos itens do menu

--- src/components/CardAluno/index.js
+++ src/components/CardAluno/index.js
@@ -1,33 +1,67 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import Menu, {MenuItem, MenuDivider} from 'react-native-material-menu';
+import Menu, {MenuItem} from 'react-native-material-menu';
 import moment from 'moment';
 require('moment/locale/pt-br');
+import firestore from '@react-native-firebase/firestore';
 import styles from './styles';
 
-const CardAluno = ({
-  aluno,
-  avaliar,
-  trocar,
-  editar,
-  onPressPlanilha,
-  toggleMenu,
-}) => {
+import {openDialog} from 'rn-android-picker-dialog';
+
+const CardAluno = ({item, avaliar, trocar, editar, navigation}) => {
   const menu = useRef(null);
+  const [distancia, setDistancia] = useState('');
+  const [aluno, setAluno] = useState(item);
 
-  const hideMenu = () => {
-    console.log('hide');
-    menu.current.hide();
+  const getList = (total, label) => {
+    return Array.apply(null, Array(total)).map(function(_, i) {
+      return i.toString() + ` ${label}`;
+    });
   };
-
+  const kms = getList(5, 'km');
+  const mts = getList(1000, 'm');
   const showMenu = () => {
-    console.log('show');
     menu.current.show();
   };
+  const avalia = async () => {
+    const inputs = [kms, mts];
+    const selectedValues = distancia ? [distancia.km, distancia.m] : [0, 0];
+    const options = {
+      dialogTitle: 'Avaliação VO2',
+    };
+    const result = await openDialog(inputs, selectedValues, options);
+    if (result) {
+      setDistancia({km: result[0], m: result[1]});
+      vo2m(result[0], result[1]);
+    }
+  };
 
-  toggleMenu(menu);
+  const vo2m = async (km, m) => {
+    let d = km * 1000 + m;
+    let vo2m = (d - 504) / 45;
+    try {
+      const user = firestore().doc(`users/${aluno.uid.trim()}`);
+      await user.update({
+        vo2: vo2m.toFixed(2),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    setAluno({...aluno, vo2: vo2m.toFixed(2)});
+  };
+  const showAvalia = () => {
+    menu.current.hide();
+    avalia();
+  };
+
+  const showPlanilha = () => {
+    menu.current.hide();
+    navigation.navigate('Vo2', {item: aluno});
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.cardAluno}>
@@ -47,15 +81,13 @@ const CardAluno = ({
         </View>
         <TouchableOpacity onPress={showMenu}>
           <View style={styles.menu}>
-            <Menu ref={menu}>
-              {trocar && <MenuItem onPress={hideMenu}>Trocar Aluno</MenuItem>}
+            <Menu ref={menu} button={<></>}>
+              {trocar && <MenuItem onPress={() => {}}>Trocar Aluno</MenuItem>}
               {editar && (
-                <MenuItem onPress={menu => onPressPlanilha(menu)}>
-                  Editar Planilha
-                </MenuItem>
+                <MenuItem onPress={showPlanilha}>Editar Planilha</MenuItem>
               )}
 
-              {avaliar && <MenuItem onPress={hideMenu}>Avaliar</MenuItem>}
+              {avaliar && <MenuItem onPress={showAvalia}>Avaliar</MenuItem>}
             </Menu>
             <Icon name="dots-vertical" size={20} />
           </View>
