049250307da19bbc20a80f22bc0c78731fa59888
Adilson Oliveira <adilsonoj@yahoo.com.br>
2020-04-06 21:21:13 -0300

    adiciona item logout

--- src/components/DrawerItens/index.js
+++ src/components/DrawerItens/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {ScrollView, Button} from 'react-native';
+import {ScrollView, Button, View, Text, TouchableOpacity} from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
 import {DrawerNavigatorItems} from 'react-navigation-drawer';
-import {NavigationActions} from 'react-navigation';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import auth from '@react-native-firebase/auth';
 import {connect} from 'react-redux';
 import * as LoginAction from '../../actions/loginAction';
@@ -11,24 +11,45 @@ import {StyleSheet} from 'react-native';
 
 const Logout = ({userLoggedOut, navigation}) => async () => {
   const user = auth().currentUser;
-  console.log('logout');
   if (user) {
     await auth().signOut();
-    console.log('logout');
-    console.log(userLoggedOut);
     await userLoggedOut();
     navigation.navigate('Login');
   }
 };
-console.log('logout');
 
-const DrawerItems = props => (
+const Sair = ({onPress}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View
+        style={{
+          paddingHorizontal: 16,
+          flexDirection: 'row',
+          height: 40,
+          alignContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Icon name="logout" size={18} />
+        <Text
+          style={{
+            paddingLeft: 40,
+            fontWeight: 'bold',
+          }}>
+          Logout
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const DrawerItems = (props) => (
   <ScrollView>
     <SafeAreaView
       style={styles.container}
       forceInset={{top: 'always', horizontal: 'never'}}>
       <DrawerNavigatorItems {...props} />
-      <Button title="Logout" onPress={Logout(props)} />
+      {/* <Button title="Logout" onPress={Logout(props)} /> */}
+      <Sair onPress={Logout(props)} />
     </SafeAreaView>
   </ScrollView>
 );
@@ -39,7 +60,7 @@ const styles = StyleSheet.create({
   },
 });
 
-const mapDispatchToProps = dispatch =>
+const mapDispatchToProps = (dispatch) =>
   bindActionCreators(LoginAction, dispatch);
 
 export default connect('', mapDispatchToProps)(DrawerItems);
